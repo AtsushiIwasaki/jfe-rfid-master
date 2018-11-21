9dcab360515f8c3cef044af5c58d47b55eb81009
iwasakat <iwasakiats@gmail.com>
2018-11-21 16:22:27 +0900

    2_routeing_and_controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+
+$head = '<html><head>';
+
+$style = <<<EOF
+<style>
+    body { font-size:16pt; color:#999; }
+    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+</style>
+EOF;
+
+$body = '</head><body>';
+$end = '</body></head>';
+
+function tag($tag, $txt) {
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
+/*
     //
     public function index() {
         return <<<EOF
@@ -24,4 +43,29 @@ class HelloController extends Controller
 </html>
 EOF;
     }
+*/
+
+
+    public function index()
+    {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Index') . $style . $body
+            . tag('h1', 'Index') . tag('p', 'thie is Index page')
+            . '<a href="/hello/other"> go to Other page</a>'
+            . $end;
+        return $html;
+    }
+
+    public function other()
+    {
+        global $head, $style, $body, $end;
+
+        $html = $head . tag('title', 'Hello/Ohter') . $style . $body
+            .tag('h1', 'Ohter') . tag('p', 'this is Other page') . $end;
+
+        return $html;
+    }
+
+
 }
