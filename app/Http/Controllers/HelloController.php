01a3df90df9cfcb645da3e336e0e4a1e47605eba
iwasakat <iwasakiats@gmail.com>
2018-11-24 17:06:10 +0900

    5-2 DBclass

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -25,113 +25,73 @@ function tag($tag, $txt) {
 
 class HelloController extends Controller
 {
-/*
-    //
-    public function index() {
-        return <<<EOF
-<html>
-    <head>
-        <title>Hello/Index</title>
-        <style>
-            body { font-size:16pt; color:#999; }
-            h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-        </style>
-    </head>
-    <body>
-        <h1>Index</h1>
-        <p>これは、HelloコントローラのIndexアクションです</p>
-    </body>
-</html>
-EOF;
-    }
-*/
 
-/*
-    public function index()
+    //index
+    public function index(Request $request)
     {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Index') . $style . $body
-            . tag('h1', 'Index') . tag('p', 'thie is Index page')
-            . '<a href="/hello/other"> go to Other page</a>'
-            . $end;
-        return $html;
-    }
-
-    public function other()
-    {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Ohter') . $style . $body
-            .tag('h1', 'Ohter') . tag('p', 'this is Other page') . $end;
-
-        return $html;
-    }
-*/
-
-/*
-    //3-1 PHPテンプレートの利用
-    public function index($id='zero')
-    {
-        //return view('hello.index');
-        $data = ['msg'=>'これはコントローラから渡されたメッセージです', 'id'=>$id];
-        return view('hello.index', $data);
-    }
-*/
-
-
-//
-
-//    public function index(Request $request)
-//    {
-//        $items = DB::select('select * from people');
-//        return view('hello.index', ['items' => $items]);
-//    }
-
-    public function index(Request $request){
-
-        //idがパラメータで指定されて入れば
-//        if(isset($request->id)){
-//            $param = ['id' => $request->id];
-//            $items = DB::select('select * from people where id = :id', $param);
-//        }
-//        else{
-            $items = DB::select('select * from people');
-//        }
-        return view('hello.index', ['items' => $items]);
-    }
-
-
-//    //3-2 Bladeテンプレートを使う
-//    //web.phpにRoute::post('hello', 'HelloController@post')を追加
-//    public function post(Request $request) {
-//
-//        $msg = $request->msg;
-//
-//        $data = [
-//            'msg' => 'こんにちは、' . $msg . 'さん！',
-//        ];
-//
-//        return view('hello.index', $data);
-//    }
-
-    public function post(Request $request){
         $items = DB::select('select * from people');
         return view('hello.index', ['items' => $items]);
     }
 
-    public function add(Request $request){
+    //index
+    public function post(Request $request)
+    {
+        $items = DB::select('select * from people');
+        return view('hello.index', ['items' => $items]);
+    }
+
+    //add
+    public function add(Request $request)
+    {
         return view('hello.add');
     }
 
-    public function create(Request $request){
+    //add execute
+    public function create(Request $request)
+    {
         $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
+        return redirect('hello');
+    }
+
+    //edit
+    public function edit(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    //edit execute
+    public function update(Request $request)
+    {
+        $param = [
+            'id'   => $request->id,
             'name' => $request->name,
             'mail' => $request->mail,
             'age'  => $request->age,
         ];
-        DB::insert('insert into people (name, mail, age) values (:name, :mail, :age)', $param);
 
-        return redirect('hello');
+        DB::update('update people set name =:name, mail =:mail, age =:age where id = :id', $param);
+        return redirect('/hello');
+    }
+
+    //delete
+    public function del(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('select * from people where id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+        $param = ['id' => $request->id];
+        DB::delete('delete from people where id = :id', $param);
+        return redirect('/hello');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('hello', 'HelloController@index');
-//Route::get('hello/other', 'HelloController@other');
-
-//Route::get('hello', function() {
-//    return view('hello.index');
-//});
 
 Route::get('hello', 'HelloController@index');
-//Route::get('hello/{id?}', 'HelloController@index');
 Route::post('hello', 'helloController@post');
 
 Route::get('hello/add', 'HelloController@add');
 Route::post('hello/add', 'HelloController@create');
 
+Route::get('hello/edit', 'HelloController@edit');
+Route::post('hello/edit', 'HelloController@update');
+
+Route::get('hello/del', 'HelloController@del');
+Route::post('hello/del', 'HelloController@remove');
 
